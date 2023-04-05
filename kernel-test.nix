4b8c87cb0cd98e34da72bed556c730fdb4a2bc2b
Cole Helbling <cole.e.helbling@outlook.com>
2023-04-05 07:14:18 -0700

    fine, I won't use a local path

--- kernel-test.nix
+++ kernel-test.nix
@@ -11,5 +11,8 @@ buildLinux (args // rec {
   # branchVersion needs to be x.y
   extraMeta.branch = versions.majorMinor version;
 
-  src = builtins.fetchGit /tmp/linux;
+  src = builtins.fetchGit {
+    url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git";
+    rev = "17d99ea98b6238e7e483fba27e8f7a7842d0f345";
+  };
 } // (args.argsOverride or { }))
